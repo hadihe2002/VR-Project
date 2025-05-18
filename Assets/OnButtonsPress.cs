baf68f6b834f8cdb44a4eb2790b4e35c022643b5
hadi <hadihe2002@gmail.com>
2025-05-18 23:20:42 +0330

    ADD: ray toggle

---
+++ Assets/OnButtonsPress.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class ButtonsTogether : MonoBehaviour
+{
+    [Tooltip("Add two or more Button-type actions here (embedded in this component).")]
+    [SerializeField] private InputAction[] actions;
+
+    public UnityEvent onAllPressed;     // fires once when every button is down
+    public UnityEvent onAnyReleased;    // fires once when the combo breaks
+
+    /* ------------------ life-cycle ------------------ */
+
+    private void OnEnable()
+    {
+        foreach (var a in actions)
+        {
+            a.performed += OnPerformed;   // button crossed the press threshold
+            a.canceled += OnCanceled;    // button released
+            a.Enable();                   // IMPORTANT - start listening
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var a in actions)
+        {
+            a.performed -= OnPerformed;
+            a.canceled -= OnCanceled;
+            a.Disable();
+        }
+    }
+
+    /* ------------------ callbacks ------------------- */
+
+    private void OnPerformed(InputAction.CallbackContext _)
+    {
+        if (AllPressed())           // have we just reached the full set?
+            onAllPressed.Invoke();
+    }
+
+    private void OnCanceled(InputAction.CallbackContext _)
+    {
+        if (!AllPressed())          // have we just LEFT the full set?
+            onAnyReleased.Invoke();
+    }
+
+    private bool AllPressed()
+    {
+        foreach (var a in actions)
+            if (!a.IsPressed())     // IsPressed() works for any button interaction
+                return false;
+        return true;
+    }
+}
